6d15c3603e6462615054b40a9c8e1653d3da9176
being-kashiwagi <kashiwagi@beingcorp.co.jp>
2022-09-29 17:26:58 +0900

    Pullrequests指摘箇所の修正
    メソッドコメントの削除
    
    変数名、メソッド名の修正
    
    escape処理の実行箇所修正

--- main.rb
+++ main.rb
@@ -5,97 +5,86 @@ require 'sinatra/reloader'
 require 'json'
 require 'cgi/escape'
 
-# メモ保存ファイルから全てのメモ情報を取得する
-# @return memo_data:全てのメモデータ
-def read_all_memodata
-  memo_data = []
-  File.open('./public/memo_data.json') do |f|
-    memo_data = JSON.parse(f.read)
+def read_all_memos
+  memo = []
+  File.open('./public/memo.json') do |f|
+    memo = JSON.parse(f.read)
   end
-  memo_data
+  memo
 end
 
-# メモ保存ファイルからidが一致するメモを取得する
-# @param id:メモid
-# @param memo_data:全てのメモデータ(read_all_memodataの結果)
-# @return title:idに一致したメモのタイトル
-# @return content:idに一致したメモの内容
-def read_pick_memodata(id, memo_data)
+def read_memo(id, memo)
   title = ''
   content = ''
-  memo_data.length.times do |i|
-    if memo_data[i]['id'].to_s == id.to_s
-      title = memo_data[i]['title']
-      content = memo_data[i]['content']
+  memo.length.times do |i|
+    if memo[i]['id'].to_s == id.to_s
+      title = memo[i]['title']
+      content = memo[i]['content']
     end
   end
-  [title, content]
+  { title: title, content: content }
 end
 
-# トップページを取得する
 get '/' do
-  @memo_data = read_all_memodata
+  @memos = read_all_memos
   erb :top_page
 end
 
-# メモ新規作成ページを取得する
 get '/memo/create-page' do
   erb :create_page
 end
 
-# メモ内容表示ページを取得する
 get '/memo/detail-page/:id' do
   @id = params[:id]
-  @title, @content = read_pick_memodata(params[:id], read_all_memodata)
+  memo = read_memo(params[:id], read_all_memos)
+  @title = CGI.escapeHTML(memo[:title])
+  @content = CGI.escapeHTML(memo[:content]).gsub(/\R/, '<br>')
   erb :detail_page
 end
 
-# メモ内容編集ページを取得する
 get '/memo/edit-page' do
   @id = params[:id]
-  @title, @content = read_pick_memodata(params[:id], read_all_memodata)
+  memo = read_memo(params[:id], read_all_memos)
+  @title = CGI.escapeHTML(memo[:title])
+  @content = CGI.escapeHTML(memo[:content]).gsub(/\R/, '<br>')
   erb :edit_page
 end
 
-# メモを登録する
 post '/memo' do
-  memo_data = read_all_memodata
-  File.open('./public/memo_data.json', 'w') do |f|
-    hash = { "id": params['id'], "title": CGI.escapeHTML(params['title']), "content": CGI.escapeHTML(params['content']).gsub(/\R/, '<br>') }
-    memo_data.push(hash)
-    JSON.dump(memo_data, f)
+  memo = read_all_memos
+  File.open('./public/memo.json', 'w') do |f|
+    hash = { id: params['id'], title: params['title'], content: params['content'] }
+    memo.push(hash)
+    JSON.dump(memo, f)
   end
   redirect to '/'
 end
 
-# メモを更新する
 patch '/memo' do
-  memo_data = read_all_memodata
-  memo_data.length.times do |i|
-    if memo_data[i]['id'].to_s == params['id'].to_s
-      memo_data[i]['title'] = CGI.escapeHTML(params['title'])
-      memo_data[i]['content'] = CGI.escapeHTML(params['content']).gsub(/\R/, '<br>')
+  memo = read_all_memos
+  memo.length.times do |i|
+    if memo[i]['id'].to_s == params['id'].to_s
+      memo[i]['title'] = params['title']
+      memo[i]['content'] = params['content']
     end
   end
-  File.open('./public/memo_data.json', 'w') do |f|
-    JSON.dump(memo_data, f)
+  File.open('./public/memo.json', 'w') do |f|
+    JSON.dump(memo, f)
   end
   redirect to '/'
 end
 
-# メモを削除する
 delete '/memo' do
-  memo_data = read_all_memodata
-  memo_data.delete_if do |memo|
+  memos = read_all_memos
+  memos.delete_if do |memo|
     memo['id'] == params['id']
   end
-  File.open('./public/memo_data.json', 'w') do |f|
-    JSON.dump(memo_data, f)
+  File.open('./public/memo.json', 'w') do |f|
+    JSON.dump(memos, f)
   end
   redirect to '/'
 end
 
-# 存在しないURLアクセスへの対応
 not_found do
   '404 Not Found'
 end
